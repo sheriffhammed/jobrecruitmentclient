177a47dd9746cd1e26ec79168df285e21d29214b
Sheriff Hammed <108693777+sheriffhammed@users.noreply.github.com>
2025-02-09 23:15:51 +0000

    react/prop-types turned off

--- src/features/Joblisting/Components/JobListing.jsx
+++ src/features/Joblisting/Components/JobListing.jsx
@@ -1,10 +1,11 @@
+//eslint-disable react/prop-types;
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import TimeAgo from "../../Components/TimeAgo";
 import ApplicationButton from "../../Applications/Components/ApplicationButton";
 import ActionButtons from "./ActionButtons";
-import {useSelector } from 'react-redux'
-import { selectRoles} from "../../auth/authSlice"
+import { useSelector } from "react-redux";
+import { selectRoles } from "../../auth/authSlice";
 const JobListing = ({
   jobId,
   title,
@@ -17,28 +18,33 @@ const JobListing = ({
   jobType,
   experience,
 }) => {
-  const roles = useSelector(selectRoles)
-  
-  let actionButtons
-  actionButtons = !roles ? <ApplicationButton jobId={jobId}/> 
-                  : roles.includes(100) ? <ActionButtons jobId={jobId}/>  
-                  : <ApplicationButton jobId={jobId}/> 
+  const roles = useSelector(selectRoles);
+
+  let actionButtons;
+  actionButtons = !roles ? (
+    <ApplicationButton jobId={jobId} />
+  ) : roles.includes(100) ? (
+    <ActionButtons jobId={jobId} />
+  ) : (
+    <ApplicationButton jobId={jobId} />
+  );
   return (
     <>
       <div class="list-group-item py-3">
-          <h5 class="mb-1">{title}</h5>
-          <div>
-            <p class="mb-1 fw-bold">
-              {company} -  {location} ({modeOfWork} | {jobType} | {experience})
-            </p>
-            <p class="mb-1">
-              {description}
-            </p>
-           <p>Salary : £ {salary}</p>
-            {actionButtons}
-            </div>
-          <small>Posted<TimeAgo timestamp={createdAt} /></small>
+        <h5 class="mb-1">{title}</h5>
+        <div>
+          <p class="mb-1 fw-bold">
+            {company} - {location} ({modeOfWork} | {jobType} | {experience})
+          </p>
+          <p class="mb-1">{description}</p>
+          <p>Salary : £ {salary}</p>
+          {actionButtons}
         </div>
+        <small>
+          Posted
+          <TimeAgo timestamp={createdAt} />
+        </small>
+      </div>
     </>
   );
 };
