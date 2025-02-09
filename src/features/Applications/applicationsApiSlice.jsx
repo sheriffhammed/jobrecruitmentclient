85097e4e5e2956d5fc2fd9409b24299047288407
Sheriff Hammed <108693777+sheriffhammed@users.noreply.github.com>
2025-02-09 23:25:20 +0000

    changed directory

--- src/features/Applications/applicationsApiSlice.jsx
+++ src/features/Applications/applicationsApiSlice.jsx
@@ -1,46 +1,46 @@
-import { apiSlice } from "../../app/api/apiSlice"
+import { apiSlice } from "../../App/api/apiSlice";
 
 export const applicationsApiSlice = apiSlice.injectEndpoints({
-    endpoints: builder => ({
-         getAllApplications: builder.query({
-            query: () => '/jobapplication',
-            keepUnusedDataFor: 60,
-            providesTags: ['Application'],
-        }),
-        getApplications: builder.query({
-            query: (jobId) => `/jobapplication/${jobId}`,
-            keepUnusedDataFor: 60,
-            providesTags: ['Application'],
-        }),
-        getApplicationsByUser: builder.query({
-            query: (userId) => `/jobapplication/user/${userId}`,
-            keepUnusedDataFor: 60,
-            providesTags: ['Application'],
-        }),
-        addApplication: builder.mutation({
-            query: body => ({
-                url: '/jobapplication',
-                method: 'POST',
-                body,
-            }),
-            invalidatesTags: ['Application'],
-        }),
-         deleteApplication: builder.mutation({
-             query(id) {
-                return {
-                url: `jobapplication/${id}`,
-                method: 'DELETE',
-                }
-             },
-             invalidatesTags: ['Application'],
-         })
-    })
-})
+  endpoints: (builder) => ({
+    getAllApplications: builder.query({
+      query: () => "/jobapplication",
+      keepUnusedDataFor: 60,
+      providesTags: ["Application"],
+    }),
+    getApplications: builder.query({
+      query: (jobId) => `/jobapplication/${jobId}`,
+      keepUnusedDataFor: 60,
+      providesTags: ["Application"],
+    }),
+    getApplicationsByUser: builder.query({
+      query: (userId) => `/jobapplication/user/${userId}`,
+      keepUnusedDataFor: 60,
+      providesTags: ["Application"],
+    }),
+    addApplication: builder.mutation({
+      query: (body) => ({
+        url: "/jobapplication",
+        method: "POST",
+        body,
+      }),
+      invalidatesTags: ["Application"],
+    }),
+    deleteApplication: builder.mutation({
+      query(id) {
+        return {
+          url: `jobapplication/${id}`,
+          method: "DELETE",
+        };
+      },
+      invalidatesTags: ["Application"],
+    }),
+  }),
+});
 
 export const {
-    useGetAllApplicationsQuery,
-    useGetApplicationsQuery,
-    useGetApplicationsByUserQuery,
-    useAddApplicationMutation,
-    useDeleteApplicationMutation
-} = applicationsApiSlice 
+  useGetAllApplicationsQuery,
+  useGetApplicationsQuery,
+  useGetApplicationsByUserQuery,
+  useAddApplicationMutation,
+  useDeleteApplicationMutation,
+} = applicationsApiSlice;
